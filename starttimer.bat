d10d8477687a2db26f72a46b09e86e44320163cb
theo-ptc <131153234+theo-ptc@users.noreply.github.com>
2023-04-20 13:05:14 +0100

    #4158375
    #4158375 updated code

--- starttimer.bat
+++ starttimer.bat
@@ -1,4 +1,5 @@
 rem C:\Perl64\bin\perl.exe E:\PTC\custom_workers\VisioWorker\timer.pl
+echo start timer
 D:\PTC\ActivePerl-5.8.6.811\Perl\bin\perl.exe D:\PTC\custom_workers\AutoCADWorker\timer.pl
 
 exit
